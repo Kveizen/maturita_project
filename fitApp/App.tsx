b6d2f776f97781a19ecc0a144fda4c259f028c70
Patrik Holub <58988753+Kveizen@users.noreply.github.com>
2024-08-27 15:59:11 +0200

    Add files via upload

--- fitApp/App.tsx
+++ fitApp/App.tsx
@@ -10,10 +10,41 @@ import Profilecreen from './app/screens/tabs/ProfileScreen';
 import { useEffect, useState } from 'react';
 import { User, onAuthStateChanged } from 'firebase/auth';
 import { FIREBASE_AUTH } from './FirebaseConfig';
+import { PermissionsAndroid, Platform } from 'react-native';
+
 
 const Stack = createNativeStackNavigator();
 
 const Tab = createBottomTabNavigator();
+
+const requestBluetoothPermission = async () => {
+  if (Platform.OS === 'ios') {
+    return true
+  }
+  if (Platform.OS === 'android' && PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION) {
+    const apiLevel = parseInt(Platform.Version.toString(), 10)
+
+    if (apiLevel < 31) {
+      const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+      return granted === PermissionsAndroid.RESULTS.GRANTED
+    }
+    if (PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN && PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT) {
+      const result = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+      ])
+
+      return (
+        result['android.permission.BLUETOOTH_CONNECT'] === PermissionsAndroid.RESULTS.GRANTED &&
+        result['android.permission.BLUETOOTH_SCAN'] === PermissionsAndroid.RESULTS.GRANTED &&
+        result['android.permission.ACCESS_FINE_LOCATION'] === PermissionsAndroid.RESULTS.GRANTED
+      )
+    }
+  }
+
+  return false
+}
 
 function MyTabs() {
   return (
@@ -35,7 +66,8 @@ export default function App() {
       console.log('user', user);
       setUser(user);
     });
-  }, [])
+    requestBluetoothPermission()
+  }, []);
 
   return (
     <NavigationContainer>
